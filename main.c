25f9c584faa7b39d88c0ca68500da030ef7fa8f9
Acittttt <rasyiidraafi05@gmail.com>
2024-05-19 11:56:34 +0700

    Fix Error

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
-#include "header.h"
+#include "Rasyid.h"
 
 int main() {
-    jalankanAplikasi(); // Panggil fungsi jalankanAplikasi
+    muatDataKonser();
+
+    int pilihan;
+    do {
+        tampilkanMenu();
+        scanf("%d", &pilihan);
+        fflush(stdin);
+
+        switch (pilihan) {
+            case 1:
+                prosesLihatKonser();
+                break;
+            case 2:
+                prosesTambahKonser();
+                break;
+            case 3:
+                prosesEditKonser();
+                break;
+            case 4:
+                prosesHapusKonser();
+                break;
+            case 5:
+                printf("Keluar dari program. Sampai jumpa!\n");
+                break;
+            default:
+                printf("Menu tidak valid.\n");
+        }
+    } while (pilihan != 5);
+
     return 0;
 }
